e6cd133967d88fc673ad0f47b3c16d5ea74ddb12
unknown <1002723982@qq.com>
2023-06-04 11:28:33 +0800

    add init usrl/views

--- doc/01_步骤记录.py
+++ doc/01_步骤记录.py
@@ -181,4 +181,145 @@ class SiteUserAdmin(admin.ModelAdmin):
 
 # Alt + 回车自动导入
 admin.site.register(SiteUser,SiteUserAdmin)
+
+[04/Jun/2023 00:18:54] "GET /admin/jsi18n/ HTTP/1.1" 200 8560
+
+# 路由试图函数框架搭建
+1. 路由设计
+URL           视图views                   模板               功能
+/index/       login.views.index           index.html        首页
+/login/       login.views.login           loogin.html       登录页面
+/register/    login.views.register        register.html     注册页面
+/logout/      login.views.logout          无须返回页面        登出界面
+
+访问策略：
+    未登录人员，不论是访问index还是logout和login ,全部跳转到login页面
+    已登录人员，访问login会自动跳转到index页面
+    已登录人员，不允许直接访问register页面，需要先logout
+    登出后，自动跳转到login页面
+
+1. 主路由配置文件
+# loginRegister/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('login.urls')),    #添加的行，如果没有前缀，访问子路由配置文件
+]
+
+2. 子路由配置文件（login子应用的）
+# login/urls.py （新建的文件）
+
+from django.urls import path, include
+from login import views
+
+urlpatterns = [
+    path('index/', views.index,name='index'),
+    path('login/', views.login,name='login'),
+    path('register/', views.register,name='register'),
+    path('logout/', views.logout,name='logout'),
+]
+
+3. 视图函数配置
+# login/views.py
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def index(request):
+    pass
+    return render(request,'login/index.html')
+
+def login(request):
+    pass
+    return render(request,'login/login.html')
+
+def register(request):
+    pass
+    return render(request,'login/register.html')
+
+def logout(request):
+    pass
+    #redirect 重定向，跳转
+    return redirect('/login/')
+
+4. 模板template的配置
+#templates/login/index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+</head>
+<body>
+<h1>这是首页的模板界面</h1>
+</body>
+</html>
+
+#templates/login/login.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>用户登录</title>
+</head>
+<body>
+<h1>用户登录</h1>
+<form>
+    用户名：<input type="text" placeholder="username"><br/>
+    密码：<input type="password" placeholder="password"><br/>
+    <input type="submit" placeholder="登录">
+</form>
+</body>
+</html>
+
+#templates/login/register.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>用户注册</title>
+</head>
+<body>
+<h1>用户注册页面</h1>
+<form>
+    用户名：<input type="text" placeholder="username"><br/>
+    电子邮箱：<input type="email" placeholder="email"><br/>
+    密码：<input type="password" placeholder="password"><br/>
+    确认密码：<input type="password" placeholder="password"><br/>
+    <input type="submit" placeholder="注册">
+</form>
+</body>
+</html>
+
+5. 测试 （浏览器访问）
+http://127.0.0.1:8888/register/
+http://127.0.0.1:8888/login/
+http://127.0.0.1:8888/index/
+"""
+
+"""
+git init
+git log
+git add *
+git commit -m " add init  usl/views"
+git status
+
+(loginRegister) PS C:\Users\ZXK\PycharmProjects\day_08\loginRegister> git status
+On branch master                                   
+Changes to be committed:                           
+  (use "git restore --staged <file>..." to unstage)
+        new file:   login/urls.py             
+        new file:   templates/login/index.html
+        new file:   templates/login/login.html
+        modified:   "doc/01_\346\255\245\351\252\244\350\256\260\345\275\225.py"
+        modified:   login/urls.py
+        modified:   login/views.py
+        modified:   loginRegister/urls.py
+        modified:   templates/login/index.html
+        modified:   templates/login/login.html
+
+git diff login/views.py     #查看变化
 """
--- login/urls.py
+++ login/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path, include
+from login import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('login.urls')),
+    path('index/', views.index,name='index'),
+    path('login/', views.login,name='login'),
+    path('register/', views.register,name='register'),
+    path('logout/', views.logout,name='logout'),
 ]
